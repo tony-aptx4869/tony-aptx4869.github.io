8390583d2e60e2afa19f50d4ad54c52fbe8bd240
Tony Chang <11223617+tony-aptx4869@users.noreply.github.com>
2021-07-02 20:24:12 +0800

    Site updated: 2021-07-02 20:24:11

---
+++ dist/player.js
@@ -0,0 +1,19 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: auto,
+    audio: [
+      {
+        name: "君の涙にこんなに恋してる",
+        artist: '夏いる',
+        url: 'https://aptx4869.tv/music/natuiru-kimino_namidani_konnani_koisiteru.mp3',
+        cover: 'https://aptx4869.tv/images/music_covers/natuiru-kimino_namidani_konnani_koisiteru_cover.jpg',
+      },
+      {
+        name: '君がいない夏',
+        artist: 'DEEN',
+        url: 'https://aptx4869.tv/music/deen-kimiga_inai_natu.mp3',
+        cover: 'https://aptx4869.tv/images/music_covers/deen-kimiga_inai_natu_cover.jpg',
+      }
+    ]
+});
