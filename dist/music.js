b8148c4b75aea1c352cd51ff8be23dfff3a6a69a
Tony Chang <11223617+tony-aptx4869@users.noreply.github.com>
2021-07-04 19:15:48 +0800

    Site updated: 2021-07-04 19:15:47

--- dist/music.js
+++ dist/music.js
@@ -19,43 +19,43 @@ const ap = new APlayer({
         name: 'Thêm Một Lần Đau (错错错越南版本 aka 太木人道)',
         artist: 'HKT',
         url: 'https://aptx4869.tv/music/HKT-themmotlandau.mp3',
-        cover: 'https://aptx4869.tv/images/music_covers/HKT-themmotlandau_cover.png',
+        cover: 'https://aptx4869.tv/images/music_covers/HKT-themmotlandau_cover.jpg',
       },
       {
         name: 'สิ่งเล็กเล็กที่เรียกว่ารัก (初恋这件小事)',
         artist: 'Wan Thanakrit',
         url: 'https://aptx4869.tv/music/Wan_Thanakrit-singleklekthireiykwarak.mp3',
-        cover: 'https://aptx4869.tv/images/music_covers/movie_soundtrack-singleklekthireiykwarak_cover.jpg',
+        cover: 'https://aptx4869.tv/images/music_covers/singleklekthireiykwarak_cover.jpg',
       },
       {
         name: 'วันเดือนปี (日月年)',
         artist: 'Jeab Wattana',
         url: 'https://aptx4869.tv/music/Jeab_Wattana-wandeuxnpi.mp3',
-        cover: 'https://aptx4869.tv/images/music_covers/movie_soundtrack-singleklekthireiykwarak_cover.jpg',
+        cover: 'https://aptx4869.tv/images/music_covers/singleklekthireiykwarak_cover.jpg',
       },
       {
         name: 'เพราะใจ (因为心)',
         artist: 'Jeab Wattana',
         url: 'https://aptx4869.tv/music/Jeab_Wattana-pheraaci.mp3',
-        cover: 'https://aptx4869.tv/images/music_covers/movie_soundtrack-singleklekthireiykwarak_cover.jpg',
+        cover: 'https://aptx4869.tv/images/music_covers/singleklekthireiykwarak_cover.jpg',
       },
       {
         name: 'สักวันหนึ่ง (有一天)',
         artist: 'Marisa Sukosol Nunphakdit',
         url: 'https://aptx4869.tv/music/Marisa_Sukosol_Nunphakdit-sakwanhnung.mp3',
-        cover: 'https://aptx4869.tv/images/music_covers/movie_soundtrack-singleklekthireiykwarak_cover.jpg',
+        cover: 'https://aptx4869.tv/images/music_covers/singleklekthireiykwarak_cover.jpg',
       },
       {
         name: 'สักวันฉันจะดีพอ (总有一天我会足够优秀)',
         artist: 'Bodyslam',
         url: 'https://aptx4869.tv/music/Bodyslam-sakwanchancadiphx.mp3',
-        cover: 'https://aptx4869.tv/images/music_covers/movie_soundtrack-singleklekthireiykwarak_cover.jpg',
+        cover: 'https://aptx4869.tv/images/music_covers/singleklekthireiykwarak_cover.jpg',
       },
       {
         name: 'สิ่งเล็กเล็กที่เรียกว่ารัก OS (初恋这件小事 原声)',
         artist: 'V.A.',
         url: 'https://aptx4869.tv/music/VA-singleklekthireiykwarak_OS.mp3',
-        cover: 'https://aptx4869.tv/images/music_covers/movie_soundtrack-singleklekthireiykwarak_cover.jpg',
+        cover: 'https://aptx4869.tv/images/music_covers/singleklekthireiykwarak_cover.jpg',
       }
     ]
 });
